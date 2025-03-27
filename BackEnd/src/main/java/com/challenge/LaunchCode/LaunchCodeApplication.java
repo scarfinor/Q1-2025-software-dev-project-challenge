6cd46696dc5f293303b747765f0f273c57457819
scarfinor <scarfinor@gmail.com>
2025-03-26 21:39:26 -0500

    vite project build complete

---
+++ BackEnd/src/main/java/com/challenge/LaunchCode/LaunchCodeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.LaunchCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchCodeApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/challenge/LaunchCode/LaunchCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.LaunchCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
